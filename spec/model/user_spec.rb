8f0506b66eecdeedbba6e59275441c1087957cc4
Peter1907 <peter.nady.1408@gmail.com>
2022-12-10 23:13:13 +0200

    Fix linter errors

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user_1 = User.new(name: 'user_1', photo: 'photo_1', bio: 'bio_1')
+  user = User.new(name: 'user_1', photo: 'photo_1', bio: 'bio_1')
 
-  before { user_1.save }
+  before { user.save }
 
   it 'should have a name' do
-    user_1.name = nil
-    expect(user_1).to_not be_valid
+    user.name = nil
+    expect(user).to_not be_valid
   end
 
   it 'should have a post_counter greater than or equal to 0' do
-    expect(user_1.posts_counter).to be >= 0
+    expect(user.posts_counter).to be >= 0
   end
 end
