8d67fa621b9739fb030f115e1643a76a270d616b
Peter1907 <peter.nady.1408@gmail.com>
2022-12-12 21:50:04 +0200

    Add controllers & simple views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users, only: %i[show index] do
+    resources :posts, only: %i[show index]
+  end
 end
