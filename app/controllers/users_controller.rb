4c198d29b363f2f272d9de0951cf026800ee7729
Peter1907 <peter.nady.1408@gmail.com>
2022-12-15 23:46:58 +0200

    Update pages with basic layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def show
-    @id = params[:id]
+    @user = User.find_by(id: params[:id])
   end
 end
