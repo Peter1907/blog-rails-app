17c9b7962a18fe35b136272ab119ceed2c655c88
Peter1907 <peter.nady.1408@gmail.com>
2022-12-25 00:44:42 +0200

    Update the seeds.rb file as per the code review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: second_user, title: 'This is my second post!', text: 'Second post is always better than the first one')
+third_post = Post.create(author: first_user, title: 'Going again!', text: 'We\'re going to the moon!')
+fourth_post = Post.create(author: second_user, title: 'Second on the run', text: 'This is my second post')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
+Comment.create(post: first_post, author: second_user, text: 'Hello Tom')
+Comment.create(post: first_post, author: second_user, text: 'Hola Tom')
+Comment.create(post: first_post, author: second_user, text: 'Wassup Tom')
+Comment.create(post: first_post, author: second_user, text: 'How are you, Tom?')
+Comment.create(post: first_post, author: second_user, text: 'Good to see you, Tom')
