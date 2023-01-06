18197de996774bcbb9c71394278f930924049078
Peter1907 <peter.nady.1408@gmail.com>
2023-01-06 11:10:43 +0200

    Integrate devise with the user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
 
   resources :users, only: %i[show index] do
     resources :posts, only: %i[show index new create] do
